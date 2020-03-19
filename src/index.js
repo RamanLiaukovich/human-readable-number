3a1ab9d38a4b8a2689b239e627317e9b5cc4fe6f
Raman Liakovich <2038634@gmail.com>
2020-03-19 03:09:59 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function toReadable (number) {
-  
+    let num = +number;
+  let units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  let elen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let dozens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    
+  let readable = [];
+  let hundreds = Math.floor(num / 100);
+  let twen = num % 100;
+  let eden = twen % 10;
+  if (number === 0) return ['zero'];
+  if (hundreds > 0) {
+    readable.push(units[hundreds]);
+    readable.push('hundred');
+}
+  if (twen >= 20) {
+    readable.push(dozens[Math.floor(twen / 10)]);
+    if (eden !== 0) readable.push(units[eden]);
+  }
+  else if (twen >= 10) readable.push(elen[twen % 10]);
+  else if (twen !== 0) readable.push(units[eden]);
+  return readable.join(' ');
 }
